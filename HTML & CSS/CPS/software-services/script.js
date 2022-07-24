7080d7c2f279c2544fdbaf0a15c07d8bc020a91d
local-leha <vegaszurized@gmail.com>
2022-07-24 12:34:10 +0300

    section created without slider

---
+++ HTML & CSS/CPS/software-services/script.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const brands = document.querySelectorAll('.brands__item') 
+    const showMore = document.querySelector('.show-more')
+
+    showMore.addEventListener("click", (event) => {
+        if (event.target === showMore){
+            let showMoreIcon = event.target.previousElementSibling
+            showMoreIcon.classList.toggle('transform')
+            
+            brands.forEach(brand => {
+                if (brand.classList.contains('large-size')) {
+                    brand.classList.toggle('hidden')
+                }
+                if(brand.classList.contains('med-size')){
+                    brand.classList.toggle('active')
+                }
+            })
+
+            if (showMore.textContent === 'Скрыть'){
+                showMore.innerHTML = 'Показать все'
+            } else{
+                showMore.innerHTML = 'Скрыть'
+            }
+
+        }
+    })
+})
